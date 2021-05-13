4b6e38aa0534d7a379177b0833b0f4a28fa30721
Charles <charlescampet03@gmail.com>
2021-05-13 09:40:49 +0200

    add scss and jsx base

--- webagency/src/App.jsx
+++ webagency/src/App.jsx
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import './assets/fonts/fonts.scss';
+import './style.scss';
+import Navbar from './components/Navbar';
+import { useEffect, useState } from 'react';
+import Config from './config';
+import Home from './pages/Home';
+import Works from './pages/Works';
+import About from './pages/About';
+import DayNightContext from './DayNightContext';
 
-function App() {
+const App = () => {
+  const [theme, setTheme] = useState(() => {
+    const isUserThemeDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (isUserThemeDark) {
+      return (localStorage.getItem(Config.STORAGE_KEY_THEME) || 'dark');
+    }
+    return (localStorage.getItem(Config.STORAGE_KEY_THEME) || 'light');
+  });
+
+  useEffect(() => {
+    localStorage.setItem(Config.STORAGE_KEY_THEME, theme);
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DayNightContext.Provider value={{
+      theme,
+      toggleTheme: () => {
+        if (theme === 'light') setTheme('dark');
+        if (theme === 'dark') setTheme('light');
+      },
+    }}
+    >
+      <div className={`App ${theme}`}>
+        <Router>
+          <Navbar />
+          <main>
+            <Switch>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+              <Route path="/works">
+                <Works />
+              </Route>
+              <Route path="/about" exact>
+                <About />
+              </Route>
+              <Route>
+                Page introuvable
+              </Route>
+            </Switch>
+          </main>
+        </Router>
+      </div>
+    </DayNightContext.Provider>
   );
-}
+};
 
 export default App;
--- webagency/src/index.jsx
+++ webagency/src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
 import App from './App';
+
+AOS.init();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
