fc3fa8406a2608a24352634c3bce02d36660c3fb
Charles <charlescampet03@gmail.com>
2021-05-13 09:52:53 +0200

    add data config and fonts design

---
+++ webagency/src/config/index.js
@@ -0,0 +1,6 @@
+const config = {
+    STORAGE_KEY_THEME: 'thp-next-app-agency-website__theme',
+    STORAGE_KEY_LISTGRID: 'thp-next-app-agency-website__listgrid',
+  };
+  
+  export default config;
